d8f42fbde99965dbe596c3e796a9266d40dc3572
FiveTW87 <thanwalokwattanakul@gmail.com>
2024-01-31 17:21:52 +0700

    feat/five(register):style step 1 2 3 style

--- client/src/pages/RegisterMainPage.jsx
+++ client/src/pages/RegisterMainPage.jsx
@@ -39,10 +39,10 @@ function RegisterMainPage() {
             className="flex flex-col gap-10 pb-10"
             onSubmit={formik.handleSubmit}
           >
-            <div className="text-2xl text-purple-500 pl-60 pb-6">
+            <div className="text-2xl text-purple-500 pb-6">
               Basic Information
             </div>
-            <div id="input-container-1" className="flex justify-around">
+            <div id="input-container-1" className="flex justify-between">
               <div id="name-input">
                 <div className="text-base text-black">Name</div>
                 <input
@@ -67,7 +67,7 @@ function RegisterMainPage() {
                 />
               </div>
             </div>
-            <div id="input-container-2" className="flex justify-around">
+            <div id="input-container-2" className="flex justify-between">
               <div id="location-input">
                 <div className="text-base text-black">Location</div>
                 <select
@@ -99,7 +99,7 @@ function RegisterMainPage() {
                 </select>
               </div>
             </div>
-            <div id="input-container-3" className="flex justify-around">
+            <div id="input-container-3" className="flex justify-between">
               <div id="username-input">
                 <div className="text-base text-black">Username</div>
                 <input
@@ -125,7 +125,7 @@ function RegisterMainPage() {
                 />
               </div>
             </div>
-            <div id="input-container-4" className="flex justify-around">
+            <div id="input-container-4" className="flex justify-between">
               <div id="password-input">
                 <div className="text-base text-black">Password</div>
                 <input
@@ -159,10 +159,10 @@ function RegisterMainPage() {
             className="flex flex-col gap-10 pb-10"
             onSubmit={formik.handleSubmit}
           >
-            <div className="text-2xl text-purple-500 pl-60 pb-6">
+            <div className="text-2xl text-purple-500 pb-6">
               Identities and Interests
             </div>
-            <div id="input-container-1" className="flex justify-around">
+            <div id="input-container-1" className="flex justify-between">
               <div id="gender-input">
                 <div className="text-base text-black">Sexual identities </div>
                 <select
@@ -196,7 +196,7 @@ function RegisterMainPage() {
                 </select>
               </div>
             </div>
-            <div id="input-container-2" className="flex justify-around">
+            <div id="input-container-2" className="flex justify-between">
               <div id="recial-input">
                 <div className="text-base text-black">Racial preferences</div>
                 <select
@@ -234,7 +234,7 @@ function RegisterMainPage() {
                   Hobbies / Interests (Maximum 10)
                 </div>
                 <input
-                  className="border bg-white rounded-lg p-3 w-[56.4rem] focus:border-purple-500 outline-none"
+                  className="border bg-white rounded-lg p-3 w-[66rem] focus:border-purple-500 outline-none"
                   id="hobbiesInterests"
                   name="hobbiesInterests"
                   type="text"
@@ -252,7 +252,7 @@ function RegisterMainPage() {
             className="flex flex-col gap-10 pb-10"
             onSubmit={formik.handleSubmit}
           >
-            <div className="text-2xl text-purple-500 pl-60 pb-6">
+            <div className="text-2xl text-purple-500  pb-6">
               Profile pictures
               <div className=" text-gray-800 text-base">
                 Upload at least 2 photos
@@ -274,62 +274,118 @@ function RegisterMainPage() {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col bg-main w-full h-[48rem] font-nunito">
-        <header className="flex justify-around h-36 mb-20 mt-20">
-          <div id="register-header">
-            <div className="text-sm text-beige-700">Register</div>
-            <div className="text-5xl text-purple-500">
-              Join us and start matching{" "}
+      <div className="flex flex-col items-center bg-main w-full h-[55.9375rem] font-nunito">
+        <div className=" w-[66rem] h-full">
+          <header className="flex h-36 pb-20 mt-20 items-end justify-around">
+            <div id="register-header">
+              <div className="text-sm text-beige-700 font-semibold">
+                Register
+              </div>
+              <div className="text-5xl text-purple-500 font-extrabold w-[28.3125rem]">
+                Join us and start matching{" "}
+              </div>
             </div>
-          </div>
-          <div id="header-tabs">
-            <ul className="steps">
-              <li className="step step-primary">step 1/3</li>
-              <li className="step">step 2/3</li>
-              <li className="step">step 3/3</li>
-            </ul>
-          </div>
-        </header>
-        {renderFormFields()}
-      </div>
-      <div className="flex justify-around items-center w-full h-fit bg-white pt-9 pb-8">
-        <div id="pagination-number">{currentStep}/3</div>
-        <div className="flex gap-6" id="button-container">
-          <button
-            disabled={currentStep === 1}
-            className="flex items-center text-base gap-2 text-red-500 pt-1 p-4 pl-6 pr-6 disabled:bg-none"
-            onClick={prevStep}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-            >
-              <path
-                d="M12.5275 7.33665H5.08079L8.33412 4.08332C8.59412 3.82332 8.59412 3.39665 8.33412 3.13665C8.07412 2.87665 7.65412 2.87665 7.39412 3.13665L3.00079 7.52998C2.74079 7.78998 2.74079 8.20998 3.00079 8.46998L7.39412 12.8633C7.65412 13.1233 8.07412 13.1233 8.33412 12.8633C8.59412 12.6033 8.59412 12.1833 8.33412 11.9233L5.08079 8.66998H12.5275C12.8941 8.66998 13.1941 8.36998 13.1941 8.00332C13.1941 7.63665 12.8941 7.33665 12.5275 7.33665Z"
-                fill="#C70039"
-              />
-            </svg>
-            Back
-          </button>
-          {currentStep < 3 ? (
+            <div id="header-tabs" className="flex gap-3">
+              {currentStep === 1 ? (
+                <div className="flex items-center gap-4 border-purple-500 border p-4 pr-8 rounded-2xl">
+                  <div className=" text-2xl font-bold text-purple-500 bg-gray-200 p-4 pt-2 pb-2 rounded-2xl">
+                    1
+                  </div>
+                  <div>
+                    <div className=" text-gray-700 font-medium">Step 1/3</div>
+                    <div className=" text-purple-500 font-bold">
+                      Basic Information
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex items-center gap-4 border-purple-500 border p-4  rounded-2xl">
+                  <div className=" text-2xl font-bold text-gray-600 bg-gray-200 p-4 pt-2 pb-2 rounded-2xl">
+                    1
+                  </div>
+                </div>
+              )}
+              {currentStep === 2 ? (
+                <div className="flex items-center gap-4 border-purple-500 border p-4 pr-8 rounded-2xl">
+                  <div className=" text-2xl font-bold text-purple-500 bg-gray-200 p-4 pt-2 pb-2 rounded-2xl">
+                    2
+                  </div>
+                  <div>
+                    <div className=" text-gray-700 font-medium">Step 2/3</div>
+                    <div className=" text-purple-500 font-bold">
+                      Identities and Interests
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex items-center gap-4 border-purple-500 border p-4  rounded-2xl">
+                  <div className=" text-2xl font-bold text-gray-600 bg-gray-200 p-4 pt-2 pb-2 rounded-2xl">
+                    2
+                  </div>
+                </div>
+              )}
+              {currentStep === 3 ? (
+                <div className="flex items-center gap-4 border-purple-500 border p-4 pr-8 rounded-2xl">
+                  <div className=" text-2xl font-bold text-purple-500 bg-gray-200 p-4 pt-2 pb-2 rounded-2xl">
+                    3
+                  </div>
+                  <div>
+                    <div className=" text-gray-700 font-medium">Step 3/3</div>
+                    <div className=" text-purple-500 font-bold">
+                      Profile pictures
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex items-center gap-4 border-purple-500 border p-4  rounded-2xl">
+                  <div className=" text-2xl font-bold text-gray-600 bg-gray-200 p-4 pt-2 pb-2 rounded-2xl">
+                    3
+                  </div>
+                </div>
+              )}
+            </div>
+          </header>
+          {renderFormFields()}
+        </div>
+        <div className="flex justify-around items-center w-full h-fit bg-white pt-9 pb-8">
+          <div id="pagination-number">{currentStep}/3</div>
+          <div className="flex gap-6" id="button-container">
             <button
-              type="button"
-              className="rounded-[6.1875rem] bg-red-500 text-white p-3 pl-6 pr-6 hover:bg-red-400 active:bg-red-600"
-              onClick={nextStep}
+              disabled={currentStep === 1}
+              className="flex items-center text-base gap-2 text-red-500 pt-1 p-4 pl-6 pr-6 disabled:bg-none"
+              onClick={prevStep}
             >
-              Next Step
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+              >
+                <path
+                  d="M12.5275 7.33665H5.08079L8.33412 4.08332C8.59412 3.82332 8.59412 3.39665 8.33412 3.13665C8.07412 2.87665 7.65412 2.87665 7.39412 3.13665L3.00079 7.52998C2.74079 7.78998 2.74079 8.20998 3.00079 8.46998L7.39412 12.8633C7.65412 13.1233 8.07412 13.1233 8.33412 12.8633C8.59412 12.6033 8.59412 12.1833 8.33412 11.9233L5.08079 8.66998H12.5275C12.8941 8.66998 13.1941 8.36998 13.1941 8.00332C13.1941 7.63665 12.8941 7.33665 12.5275 7.33665Z"
+                  fill="#C70039"
+                />
+              </svg>
+              Back
             </button>
-          ) : (
-            <button
-              type="submit"
-              className="rounded-[6.1875rem] bg-red-500 text-white p-3 pl-6 pr-6 hover:bg-red-400 active:bg-red-600"
-            >
-              Submit
-            </button>
-          )}
+            {currentStep < 3 ? (
+              <button
+                type="button"
+                className="rounded-[6.1875rem] bg-red-500 text-white p-3 pl-6 pr-6 hover:bg-red-400 active:bg-red-600"
+                onClick={nextStep}
+              >
+                Next Step
+              </button>
+            ) : (
+              <button
+                type="submit"
+                className="rounded-[6.1875rem] bg-red-500 text-white p-3 pl-6 pr-6 hover:bg-red-400 active:bg-red-600"
+              >
+                Submit
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </>
