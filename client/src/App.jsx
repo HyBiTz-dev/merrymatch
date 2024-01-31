dac436fc0d5a822eb21f52bf301a18169820bb8e
FiveTW87 <thanwalokwattanakul@gmail.com>
2024-01-31 14:23:34 +0700

    feat/five(register):feature register main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Homepage";
-
+import RegisterMainPage from "./pages/RegisterMainPage";
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/register" element={<RegisterMainPage />} />
         </Routes>
       </BrowserRouter>
     </>
