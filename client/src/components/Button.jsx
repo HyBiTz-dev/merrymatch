e00dbe465f56c2f06d7fb6b6ad5dde26046d4fd2
Park <tanagrit.sornchaiyagorn@gmail.com>
2024-02-01 14:46:04 +0700

    refactor/park(button and navbar jsx): refactor component code from j

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,24 +1,26 @@
-import className from "classnames";
-
-export default function Button(props) {
-  if (props.primary) {
-    const classes = className({
-      "btn bg-red-500 hover:bg-red-400 active:bg-red-600 text-white border-none rounded-btn px-6 shadow-btn1 font-bold text-base":
-        props.primary,
-    });
+export default function Button({
+  props,
+  primary,
+  secondary,
+  className,
+  children,
+  onClick,
+}) {
+  if (primary) {
+    const classes =
+      "btn bg-red-500 hover:bg-red-400 active:bg-red-600 text-white border-none rounded-btn px-6 shadow-btn1 font-bold text-base";
     return (
-      <button className={props.className + " " + classes}>
-        {props.children}
+      <button className={className + " " + classes} onClick={onClick}>
+        {children}
       </button>
     );
-  } else if (props.secondary) {
-    const classes = className({
-      "btn bg-red-100 hover:bg-red-200 active:bg-red-300 text-red-600 border-none rounded-btn px-6 shadow-btn2 font-bold text-base":
-        props.secondary,
-    });
+  } else if (secondary) {
+    const classes =
+      "btn bg-red-100 hover:bg-red-200 active:bg-red-300 text-red-600 border-none rounded-btn px-6 shadow-btn2 font-bold text-base";
+
     return (
-      <button className={props.className + " " + classes}>
-        {props.children}
+      <button className={className + " " + classes} onClick={onClick}>
+        {children}
       </button>
     );
   }
