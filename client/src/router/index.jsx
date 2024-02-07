7c7de87607ffd22353287b7d2952ec181884bba4
Mui <m.samita.tt@gmail.com>
2024-02-07 07:30:48 +0700

    feat/mui: merrylist page and card component

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -3,12 +3,13 @@ import Home from "../pages/Homepage";
 import Login from "../pages/LoginPage";
 import PackagePage from "../pages/Packagepage";
 import MatchingPage from "../pages/MatchingPage";
-import AdminPackageList from "../pages/AdminPackageList";
+// import AdminPackageList from "../pages/AdminPackageList";
 import AdminPackageCreate from "../pages/AdminPackageCreate";
 import AdminPackageEdit from "../pages/AdminPackageEdit";
 import Register from "../pages/RegisterMainPage";
 import AdminPage from "../pages/AdminTestPage";
 import Chat from "../pages/ChatPage";
+import MerryListPage from "../pages/MerryListPage";
 
 export default function Router() {
   const routes = [
@@ -55,6 +56,10 @@ export default function Router() {
     {
       path: "/admin/editpackage",
       element: <AdminPackageEdit />,
+    },
+    {
+      path: "/merrylist",
+      element: <MerryListPage />,
     },
   ];
 
