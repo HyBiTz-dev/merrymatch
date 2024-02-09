4f51c171e96c71bbd7d6f898900aded732a1c274
Park <tanagrit.sornchaiyagorn@gmail.com>
2024-02-09 11:41:03 +0700

    style/park(payment1-page): add complete style to payment1 page

--- client/src/pages/Payment1Page.jsx
+++ client/src/pages/Payment1Page.jsx
@@ -1,14 +1,111 @@
 import Navbar from "../components/Navbar";
-
+import package_cube from "../../public/images/package_cube.svg";
+import visa from "../../public/images/visa.svg";
+import master_card from "../../public/images/master_card.svg";
+import Button from "../components/Button";
 export default function Payment1Page() {
   return (
     <>
       <Navbar auth />
       <div className="payment1-page flex justify-center bg-[#FCFCFE]">
-        <section className="payment1-section bg-green-200 flex flex-col items-center w-[90rem] h-[58.5rem] top-[5.5rem]">
-          <h1 className="text-[24px] bg-slate-300">Hello Payment 1 page</h1>
-          <div className="payment1-container flex bg-fuchsia-300 w-[58rem] h-[34.625rem] mt-[5rem] left-[16rem] gap-[1.375rem]">
-            <div className="payment1-package-box flex flex-col bg-[#F6F7FC] w-[22.375rem] h-[14rem] rounded-[1.5rem] border-[0.063rem] border-[#D6D9E4] py-[2rem] px-[1.5rem] gap-[1.5rem]"></div>
+        <section className="payment1-section bg-[#FCFCFE] flex flex-col items-center w-[90rem] h-[58.5rem] top-[5.5rem]">
+          <div className="payment1-container flex bg-[#FCFCFE] w-[58rem] h-[34.625rem] mt-[5rem] left-[16rem] gap-[1.375rem]">
+            <div className="payment1-package-box flex flex-col items-center w-[22.375rem] h-[14rem] rounded-[1.5rem] border-[0.063rem] border-gray-400 bg-gray-100 py-[2rem] px-[1.5rem] gap-[1.5rem]">
+              <div className="package-box-header flex w-[19.375rem] h-[1.875] gap-[0.75rem]">
+                <img src={package_cube} alt="package_cube_icon"></img>
+                <div className="text-body1 text-gray-700">Merry Membership</div>
+              </div>
+              <div className="package-box-details flex flex-col w-[19.375rem] h-[7.875rem]">
+                <div className="package-box-details-subtitle w-full h-[3rem] flex justify-between py-[0.75rem] ">
+                  <span className="text-body2 text-gray-700 w-[10.438rem] h-[1.5rem]">
+                    Package
+                  </span>
+                  <span className="text-body2 text-gray-700 w-[10.25rem] h-[1.5rem] text-right">
+                    Price (Mountly)
+                  </span>
+                </div>
+                <div className="package-box-details-package w-full h-[4.875rem] py-[1.5rem] flex justify-between">
+                  <span className="text-body1 text-gray-900 w-[10.438rem] h-[1.875rem]">
+                    Premium
+                  </span>
+                  <span className="text-body1 text-gray-900 w-[10.438rem] h-[1.875rem] text-right">
+                    THB 59.00
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="payment-creditcard-box flex flex-col   w-[34.25rem] h-[34.625rem] rounded-[1.5rem] border-[0.063rem] border-gray-400 bg-gray-100 box-border">
+              <div className="creditcard-box-title w-full h-[4.875rem] flex justify-between p-[1.5rem] bg-gray-100 rounded-[1.5rem]">
+                <span className="w-[6.5rem] h-[1.875rem] text-body1 text-gray-700">
+                  Credit Card
+                </span>
+                <div className="credit-card-icon-container w-[6.25rem] h-[1.75rem] flex gap-[0.75rem]">
+                  <img
+                    className="w-[2.5rem] h-[1.75rem]"
+                    src={visa}
+                    alt="visa-icon"
+                  ></img>
+                  <img
+                    className="w-[3rem] h-[1.75rem]"
+                    src={master_card}
+                    alt="master-card-icon"
+                  ></img>
+                </div>
+              </div>
+              <div className="creditcard-box-details flex flex-col bg-white w-[34rem] h-[23.25rem] py-[2rem] px-[1.5rem]  box-border ">
+                <form className="flex flex-col gap-[2.5rem]">
+                  <section className="bg-white w-full h-[4.75rem] flex flex-col gap-[0.25rem]">
+                    <p className="text-body2 text-neutral-900">
+                      Card Number <span className="text-[#AF2758]">*</span>
+                    </p>
+                    <input
+                      className="w-full h-[3rem] rounded-[0.5rem] py-[0.75rem] px-[1rem]  gap-[0.5rem] bg-white  border-gray-400 border-[0.063rem] text-body2"
+                      placeholder="Number of Card"
+                    ></input>
+                  </section>
+                  <section className="bg-white w-full h-[4.75rem] flex flex-col gap-[0.25rem]">
+                    <p className="text-body2 text-neutral-900">
+                      Card Owner <span className="text-[#AF2758]">*</span>
+                    </p>
+                    <input
+                      className="w-full h-[3rem] rounded-[0.5rem] py-[0.75rem] px-[1rem]  gap-[0.5rem] bg-white border-gray-400 border-[0.063rem] text-body2"
+                      placeholder="Holder of card"
+                    ></input>
+                  </section>
+                  <section className="bg-white flex w-full h-[4.75rem] gap-[1.375rem]">
+                    <div className="expiry-date-box flex flex-col w-[14.938rem] h-[4.75rem] gap-[0.25rem]">
+                      <p className="w-full h-[1.5rem] gap-[0.25rem] text-body2 text-neutral-900">
+                        Expiry date <span className="text-[#AF2758]">*</span>
+                      </p>
+                      <input
+                        className="bg-white w-full h-[3rem] rounded-[0.5rem] border-[0.063rem] border-gray-400 py-[0.75rem] px-[1rem] gap-[0.5rem] text-body2 "
+                        placeholder="MM/YY"
+                      ></input>
+                    </div>
+                    <div className="cvc-cvv-box flex flex-col w-[14.938rem] h-[4.75rem] gap-[0.25rem]">
+                      <p className="w-full h-[1.5rem] gap-[0.25rem] text-body2 text-neutral-900">
+                        CVC/CVV <span className="text-[#AF2758]">*</span>
+                      </p>
+                      <input
+                        className="bg-white w-full h-[3rem] rounded-[0.5rem] border-[0.063rem] border-gray-400 py-[0.75rem] px-[1rem] gap-[0.5rem] text-body2 "
+                        placeholder="x x x"
+                      ></input>
+                    </div>
+                  </section>
+                </form>
+              </div>
+              <div className="creditcard-box-confirm bg-white w-[34rem] h-[6.4rem] rounded-b-[1.5rem] flex justify-between items-center border-t-[0.07rem] border-b-[0.07rem] border-gray400 pt-[1.5rem] pr-[1.5rem] pb-[2rem] pl-[1.5rem]  ">
+                <Button
+                  ghost
+                  className="w-[4.125] h-[2rem] font-[700] text-[1rem] rounded-[1rem]"
+                >
+                  Cancel
+                </Button>
+                <Button primary className="w-[11.063rem] h-[3rem]">
+                  Payment Confirm
+                </Button>
+              </div>
+            </div>
           </div>
         </section>
       </div>
