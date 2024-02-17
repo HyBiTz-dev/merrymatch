57f5f85892bbddaf648537ae5658efb8709eb444
Park <tanagrit.sornchaiyagorn@gmail.com>
2024-02-17 10:30:57 +0700

    add/park(payment1-page):add stripe

--- client/src/pages/Payment1Page.jsx
+++ client/src/pages/Payment1Page.jsx
@@ -3,7 +3,13 @@ import package_cube from "../../public/images/package_cube.svg";
 import visa from "../../public/images/visa.svg";
 import master_card from "../../public/images/master_card.svg";
 import Button from "../components/Button";
+import { loadStripe } from "@stripe/stripe-js";
+import { useStripe } from "@stripe/react-stripe-js";
+
 export default function Payment1Page() {
+  const stripeKey = import.meta.env.VITE_APP_STRIPE_KEY;
+  const stripe = loadStripe(stripeKey);
+
   return (
     <>
       <Navbar auth />
