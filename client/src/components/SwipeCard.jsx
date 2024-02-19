a2ee3b7df53adccad11fee4eacc380fb8e467f00
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2024-02-19 10:59:27 +0700

    feat/jat(matching-page):add profile swipe and fix conversation error

--- client/src/components/SwipeCard.jsx
+++ client/src/components/SwipeCard.jsx
@@ -1,4 +1,4 @@
-import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { useState } from "react";
 
 import "swiper/css";
@@ -15,8 +15,6 @@ export default function SwipeCard() {
   const [match, setMatch] = useState(false);
 
   const [showModal, setShowModal] = useState(false);
-
-  const swiper = useSwiper();
 
   const openModal = () => {
     setShowModal(true);
@@ -53,7 +51,7 @@ export default function SwipeCard() {
                   <div className="w-[38.75rem] h-[38.75rem] overflow-hidden rounded-[2rem] bg-cover flex flex-col justify-end relative">
                     <SwipeProfileImages />
                     <div className="flex justify-between items-center py-14 pr-8 pl-10 bg-gradient-to-t from-[#390741] to-transparent rounded-b-[2rem] z-10">
-                      <div className="flex justify-center items-center gap-4 ">
+                      <div className="flex justify-center items-center gap-4">
                         <span className="text-white text-headline3">
                           Danny{" "}
                           <span className="text-gray-400 text-headline3">
@@ -67,38 +65,6 @@ export default function SwipeCard() {
                           role="button"
                           onClick={openModal}
                         />
-                      </div>
-                      <div className="flex justify-between gap-5">
-                        <svg
-                          className=" fill-gray-100"
-                          width="16"
-                          height="16"
-                          viewBox="0 0 16 16"
-                          fill="none"
-                          role="button"
-                          xmlns="http://www.w3.org/2000/svg"
-                          onClick={() => swiper.slidePrev()}
-                        >
-                          <path
-                            d="M14.7915 7.00498H3.62148L8.50148 2.12498C8.89148 1.73498 8.89148 1.09498 8.50148 0.704976C8.11148 0.314976 7.48148 0.314976 7.09148 0.704976L0.501484 7.29498C0.111484 7.68498 0.111484 8.31498 0.501484 8.70498L7.09148 15.295C7.48148 15.685 8.11148 15.685 8.50148 15.295C8.89148 14.905 8.89148 14.275 8.50148 13.885L3.62148 9.00498H14.7915C15.3415 9.00498 15.7915 8.55498 15.7915 8.00498C15.7915 7.45498 15.3415 7.00498 14.7915 7.00498Z"
-                            fill="#9AA1B9"
-                          />
-                        </svg>
-                        <svg
-                          className="rotate-180 fill-gray-100"
-                          width="16"
-                          height="16"
-                          viewBox="0 0 16 16"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                          role="button"
-                          onClick={() => swiper.slideNext()}
-                        >
-                          <path
-                            d="M14.7915 7.00498H3.62148L8.50148 2.12498C8.89148 1.73498 8.89148 1.09498 8.50148 0.704976C8.11148 0.314976 7.48148 0.314976 7.09148 0.704976L0.501484 7.29498C0.111484 7.68498 0.111484 8.31498 0.501484 8.70498L7.09148 15.295C7.48148 15.685 8.11148 15.685 8.50148 15.295C8.89148 14.905 8.89148 14.275 8.50148 13.885L3.62148 9.00498H14.7915C15.3415 9.00498 15.7915 8.55498 15.7915 8.00498C15.7915 7.45498 15.3415 7.00498 14.7915 7.00498Z"
-                            fill="#9AA1B9"
-                          />
-                        </svg>
                       </div>
                     </div>
                   </div>
@@ -144,7 +110,7 @@ export default function SwipeCard() {
             modifier: 1,
             slideShadows: true,
           }}
-          modules={[EffectCoverflow, Zoom]}
+          modules={[EffectCoverflow]}
           className="mySwiper"
         >
           {mockdata.map((item, index) => (
--- client/src/components/SwipeProfileImages.jsx
+++ client/src/components/SwipeProfileImages.jsx
@@ -5,14 +5,17 @@ import "swiper/css/navigation";
 
 import { Mousewheel, Navigation } from "swiper/modules";
 
-export default function SwipeProfileImages({ prevClick, nextClick }) {
+export default function SwipeProfileImages() {
   return (
-    <div className="absolute w-[38.75rem] h-[38.75rem]">
+    <div className="absolute  w-[38.75rem] h-[38.75rem]">
       <Swiper
         allowTouchMove={false}
         mousewheel={true}
-        navigation={false}
-        modules={[Mousewheel]}
+        navigation={{
+          nextEl: "#next",
+          prevEl: "#prev",
+        }}
+        modules={[Mousewheel, Navigation]}
         className="mySwiper"
       >
         <SwiperSlide>
@@ -51,6 +54,38 @@ export default function SwipeProfileImages({ prevClick, nextClick }) {
           />
         </SwiperSlide>
       </Swiper>
+      <div className="flex justify-between gap-5 absolute z-50 bottom-[4.2rem] right-10">
+        <svg
+          id="prev"
+          className=" fill-gray-100"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          role="button"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M14.7915 7.00498H3.62148L8.50148 2.12498C8.89148 1.73498 8.89148 1.09498 8.50148 0.704976C8.11148 0.314976 7.48148 0.314976 7.09148 0.704976L0.501484 7.29498C0.111484 7.68498 0.111484 8.31498 0.501484 8.70498L7.09148 15.295C7.48148 15.685 8.11148 15.685 8.50148 15.295C8.89148 14.905 8.89148 14.275 8.50148 13.885L3.62148 9.00498H14.7915C15.3415 9.00498 15.7915 8.55498 15.7915 8.00498C15.7915 7.45498 15.3415 7.00498 14.7915 7.00498Z"
+            fill="#9AA1B9"
+          />
+        </svg>
+        <svg
+          id="next"
+          className="rotate-180 fill-gray-100"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          role="button"
+        >
+          <path
+            d="M14.7915 7.00498H3.62148L8.50148 2.12498C8.89148 1.73498 8.89148 1.09498 8.50148 0.704976C8.11148 0.314976 7.48148 0.314976 7.09148 0.704976L0.501484 7.29498C0.111484 7.68498 0.111484 8.31498 0.501484 8.70498L7.09148 15.295C7.48148 15.685 8.11148 15.685 8.50148 15.295C8.89148 14.905 8.89148 14.275 8.50148 13.885L3.62148 9.00498H14.7915C15.3415 9.00498 15.7915 8.55498 15.7915 8.00498C15.7915 7.45498 15.3415 7.00498 14.7915 7.00498Z"
+            fill="#9AA1B9"
+          />
+        </svg>
+      </div>
     </div>
   );
 }
