5cfcf7a069129dd4df7f065b6fd58f3369600da3
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2024-02-29 11:49:38 +0700

    fix:resovle cors allow error

--- server/app.js
+++ server/app.js
@@ -29,7 +29,11 @@ async function init() {
 
   const io = new Server(server, {
     cors: {
+<<<<<<< HEAD
       origins: process.env.BASE_URL_CLIENT,
+=======
+      origin: process.env.BASE_URL_CLIENT,
+>>>>>>> 3a3758c (fix:resovle cors allow error)
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     },
